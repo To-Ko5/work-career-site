b46b539a8ff864ded362c7855e34cc5f61731f7a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-07 21:48:24 +0900

    feat(type): types add

--- types/data.ts
+++ types/data.ts
@@ -2,18 +2,19 @@ import { LucideIcon } from 'lucide-react'
 
 export type work = {
   case: number
-  project: string
-  date: {
-    start: string
-    end: string
-  }
+  title: string
+  startDate: string
+  endDate: string
   positions: string[]
   skills: string[]
   tools: string[]
-  description: {
-    text: string
-    list: string[] | null
-  }
+  description: string
+}
+
+export type about = {
+  name: string
+  position: string
+  content: string
 }
 
 export type url_link = {
