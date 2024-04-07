6fb490614509d60690e441a96d47ad8e73e7d7d9
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-07 21:46:12 +0900

    feat(top): page add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
-import { Button } from '@/components/ui/button'
+import About from '@/components/original/about'
+import Date from '@/components/original/date'
+import Profile from '@/components/original/profile'
+import Works from '@/components/original/works'
+import { getAbout, getWorkList } from '@/lib/newt'
+import { Metadata } from 'next'
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'Newt・Next.jsブログ',
+  description: 'NewtとNext.jsを利用したブログです'
+}
+
+export default async function Home() {
+  const works = await getWorkList()
+  const about = await getAbout()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container max-w-4xl pb-16 space-y-10">
       <div>
-        <Button>test</Button>
+        <Date />
+      </div>
+      <div>
+        <Profile profile={about} />
+      </div>
+      <div>
+        <About content={about.content} />
+      </div>
+      <div>
+        <Works works={works} />
       </div>
     </main>
   )
