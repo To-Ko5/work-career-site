feffa93ce0c9435ccc0d747f62967064df01c37b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-08 15:22:55 +0900

    fix(config): config setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
-  basePath: process.env.NODE_ENV === 'production' ? '/work-career-site' : '',
-  images: {
-    unoptimized: true
-  }
-}
+const nextConfig = {}
 
 export default nextConfig
