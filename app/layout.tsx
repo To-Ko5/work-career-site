d05d1ed2fd35c6db2515bd85f73244cd5df2c840
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-08 15:10:26 +0900

    fix(layout): type fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,9 @@ type Layout = {
   modal: React.ReactNode
 }
 
-export default function RootLayout({ children }: Layout) {
+export default function RootLayout({
+  children
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ja" suppressHydrationWarning>
       <body>
