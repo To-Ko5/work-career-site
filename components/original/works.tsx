031481cddf8e7451e01fa6014aa799c233c6c9fa
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-08 11:15:01 +0900

    fix(works): bug fix

--- components/original/works.tsx
+++ components/original/works.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Work from './work'
-import { works } from '@/data/data'
+import Work from '@/components/original/work'
+import { work } from '@/types/data'
 
-const Works = () => {
+type Props = {
+  works: work[]
+}
+
+const Works = ({ works }: Props) => {
   return (
     <section>
       <h2 className="text-xl font-bold mb-4">Works</h2>
 
       <div className="space-y-8">
         {works.map((work) => {
-          return <Work key={work.case} work={work} isLink={false} />
+          return <Work key={work.case} work={work} isLink={true} />
         })}
       </div>
     </section>
