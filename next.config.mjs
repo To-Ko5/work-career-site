4ab49e2085809a4ff71e987d7ee063079700880d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-08 14:47:29 +0900

    feat(config): output add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone',
+  basePath: process.env.NODE_ENV === 'production' ? '/work-career-site' : '',
+  images: {
+    unoptimized: true
+  }
+}
 
 export default nextConfig
