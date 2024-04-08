173fdb6af54b098430ea2d3355c4f36468d72c1f
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-04-08 16:11:10 +0900

    refactor(deploy): deploy setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone'
+  // output: process.env.NODE_ENV === 'production' ? 'export' : 'standalone'
 }
 
 export default nextConfig
